ec782554a5470fec2a62f4731e5b5c5af10bb5a2
daniaalrababa9 <rababadania@gmail.com>
2020-01-22 02:06:54 +0200

    done

--- classes/vehicle-class.js
+++ classes/vehicle-class.js
@@ -1,6 +1,44 @@
 'use strict';
 
-class Car {
+class Vehicle {
+    constructor(name ,wheels){
+        this.name;
+        this.wheels=wheels
+    }
+   drive(){
+       return 'Moving Forward'
+   }
+   stop(){
+       return 'Stopping'
+   }
+}
+class Car extends Vehicle{
+    constructor(name,wheels){
+
+        super(name,4)
+    }
+
+    run(){
+        super.drive()
+    }
+    run(){
+        super.stop()
+    }
+}
+class Motorcycle extends Vehicle{
+  constructor(name,wheels){
+      super(name,2)
+  }
+    
+  run(){
+    super.drive()
+}
+run(){
+    super.stop()
+}
+wheeelie(){
+    return 'wheee!'
+}
 }
 
-module.exports = Car;
+module.exports = {Car,Motorcycle};
